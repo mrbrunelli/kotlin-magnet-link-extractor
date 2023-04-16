f118c1b4643da1da321eaab76557da0ca27cdbf1
mrbrunelli <matheus.brunelli@gmail.com>
2023-04-16 17:27:22 -0300

    add magnet link extractor

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import org.jsoup.Jsoup
+
+fun getMagnetLinks(url: String): List<String> {
+    val doc = Jsoup.connect(url).get()
+    val links = mutableListOf<String>()
+    val elements = doc.select("a")
+
+    for (element in elements) {
+        val href = element.attr("href")
+        if (href.contains("magnet:?xt")) {
+            links.add(href)
+        }
+    }
+
+    return links
+}
+
+fun main(args: Array<String>) {
+    val links = getMagnetLinks("https://flixtorrents.com.br/john-wick-de-volta-ao-jogo-2014-torrent-dublado-e-legendado/")
+    println(links.joinToString(separator = "\n"))
+}
